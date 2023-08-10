99434a639ec746326f771e723c74c4f61c4c8907
yoonseopkim <qc70045@gmail.com>
2023-08-10 15:23:48 +0900

    [Bronze III] Title: 전자레인지, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/10162. 전자레인지/전자레인지.py
+++ 백준/Bronze/10162. 전자레인지/전자레인지.py
@@ -2,33 +2,21 @@ T = int(input())
 a = 300
 b = 60
 c = 10
-a_counter = 0
-b_counter = 0
-c_counter = 0
-change = 0
-change1 = 0
+a_count = 0
+b_count = 0
+c_count = 0
 
-if T >=a:
-    a_counter = T// a
-    T = T - a_counter * a
-
-if T >= b:
-    b_counter = T // b
-    T = T - b_counter * b
-if T >= c:
-    c_counter = T // c
-    T = T - c_counter * c
+if T >= a:
+    a_count = T // a
+    T %= a
+if T>=b:
+    b_count = T // b
+    T %= b
+if T>=c:
+    c_count = T // c
+    T %= c
 if T==0:
-    print(a_counter,b_counter,c_counter)
+
+    print(a_count,b_count,c_count)
 else:
     print(-1)
-
-
-
-
-
-
-
-
-
-
