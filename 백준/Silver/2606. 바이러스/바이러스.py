4848f72dd9723b981cc7f8fd344b578dad92f184
yoonseopkim <qc70045@gmail.com>
2023-08-10 16:29:40 +0900

    [Silver III] Title: 바이러스, Time: 120 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,21 +1,19 @@
 com = int(input())
 network = int(input())
 
-graph = [[] for _ in range(com+1)]
-visited = [False] * (com+1)
-
+graph = [[] for _ in range(com + 1)]
 for _ in range(network):
-    n,m = list(map(int,input().split()))
-    # 그래프선언, 양방향그래프
+    a,b = list(map(int,input().split()))
+    graph[a].append(b) #무방향 그래프
+    graph[b].append(a)
 
-    graph[n].append(m)  # 양방향 그래프
-    graph[m].append(n)
-
+visited = [False] * (com + 1)
 def dfs(start):
+    infected = 0
     visited[start] = True
-    infected = 1
     for adj in graph[start]:
         if not visited[adj]:
             infected += dfs(adj)
-    return infected
+    return infected + 1
+
 print(dfs(1)-1)
