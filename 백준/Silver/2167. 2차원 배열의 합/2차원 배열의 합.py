4fef99aa01d0404412c38f33eeda854c89d321d1
yoonseopkim <qc70045@gmail.com>
2023-08-17 16:21:58 +0900

    [Silver V] Title: 2차원 배열의 합, Time: 1000 ms, Memory: 37032 KB -BaekjoonHub

--- 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.py
+++ 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.py
@@ -1,12 +1,19 @@
-N, M = map(int,input().split())
-lsd = [list(map(int,input().split())) for _ in range(N)]
+N, M = map(int, input().split())
+
+# 원래 배열
+arr = [list(map(int, input().split())) for _ in range(N)]
+
+# 2차원 Prefix Sum 배열 초기화
+P = [[0] * (M+1) for _ in range(N+1)]
+
+# Prefix Sum 배열 계산
+for i in range(1, N+1):
+    for j in range(1, M+1):
+        P[i][j] = P[i-1][j] + P[i][j-1] - P[i-1][j-1] + arr[i-1][j-1]
 
 K = int(input())
+
 for _ in range(K):
-    i,j,x,y = map(int,input().split())
-    count=  0
-    for n in range(i-1, x):
-        for m in range(j-1, y):
-            count += lsd[n][m]
-            
-    print(count)
+    i, j, x, y = map(int, input().split())
+    result = P[x][y] - P[i-1][y] - P[x][j-1] + P[i-1][j-1]
+    print(result)
