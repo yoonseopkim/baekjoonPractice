bdba3070c0805786ad3fef8a7203d8b4b4dd4481
yoonseopkim <qc70045@gmail.com>
2023-12-22 15:01:05 +0900

    [Bronze V] Title: A/B, Time: 212 ms, Memory: 17776 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String [] args) {
+            Scanner scanner = new Scanner(System.in);
+        double n  = scanner.nextInt();
+        double m  = scanner.nextInt();
+        double sum = n/m;
+        System.out.println(sum);
+        scanner.close();
+}
+}
