7f6978c7f78bdb6b9655622bd1cda262f99e5b79
yoonseopkim <qc70045@gmail.com>
2024-08-28 09:55:52 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.60 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
@@ -1,14 +1,11 @@
 def solution(n):
     answer = 0
-    if n <= 2:
-        return n 
-#     dp 테이블 초기화
-    dp = [0] * (n+1)
-    dp[1], dp[2] = 1,2
-
-    
-    for i in range(3, n+1):
+    if n<= 2:
+        return n
+    dp = [0] * (n+ 1)
+    dp[1], dp[2] = 1, 2
+#     3부터 해야 됌
+    for i in range(3, n +1):
+#         모듈려 연산 누락
         dp[i] = (dp[i-1] + dp[i-2]) % 1234567
-
-        
     return dp[n]
