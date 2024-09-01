59dadec894df1c231d97b843d2e63ca2c51e4152
yoonseopkim <qc70045@gmail.com>
2024-09-01 15:05:51 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
@@ -1,10 +1,8 @@
 def solution(n,a,b):
     answer = 0
-
-    
     while a != b:
-        answer += 1
-#     한 라운드가 끝날떄마다 수가 반으로 줄어든다 토너먼트라
-        a = (a+1) //2
-        b = (b+1 ) // 2
+        
+        a= (a+1) // 2
+        b = (b+1) //2
+        answer += 1 
     return answer
