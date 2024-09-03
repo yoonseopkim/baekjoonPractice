cfaab14e48d753ac055db815efa594f630e69f57
yoonseopkim <qc70045@gmail.com>
2024-09-03 09:31:33 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 13.90 ms, Memory: 11.2 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,25 +1,21 @@
 from collections import deque
 def solution(maps):
-#     bfs
-    n = len(maps)  #행의 개수
-    m = len(maps[0]) #열의 개수
+    n, m = len(maps), len(maps[0])
     dx = [-1,1,0,0]
     dy = [0,0,-1,1]
-
-    # 처음 시작지점으로 초기화
     queue = deque([(0,0,1)])
-    # 방문여부를 말하는 것
     visited = set([(0,0)])
-    while queue:
-        x,y,distance = queue.popleft()
-#         목적자에 도척
-        if x == n -1 and y == m -1:
-            return distance
-        for i in range(4):
-            nx, ny = x + dx[i], y+dy[i]
-            
-            if 0 <= nx < n and 0 <= ny < m and maps[nx][ny] == 1 and (nx,ny) not in visited:
-                queue.append((nx,ny,distance+1))
-                visited.add((nx,ny))
 
+    while queue:
+        x,y, dist = queue.popleft()
+        if x == n-1 and y == m-1:
+            return dist
+        
+        for i in range(4):
+            nx,ny = x + dx[i], y + dy[i]
+            
+            if 0 <= nx < n and 0 <=ny < m and (nx,ny) not in visited and maps[nx][ny]== 1:
+                queue.append((nx,ny,dist + 1))
+                visited.add((nx,ny))
+        
     return -1
