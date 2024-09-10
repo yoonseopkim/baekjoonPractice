1fdacb7d4498aae238009ee849259b4ec3445938
yoonseopkim <qc70045@gmail.com>
2024-09-10 14:07:23 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 15.07 ms, Memory: 11.1 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,21 +1,21 @@
 from collections import deque
 def solution(maps):
-    n, m = len(maps), len(maps[0])
+    n = len(maps)
+    m = len(maps[0])
     dx = [-1,1,0,0]
     dy = [0,0,-1,1]
     queue = deque([(0,0,1)])
     visited = set([(0,0)])
-
     while queue:
-        x,y, dist = queue.popleft()
+        x,y,distance = queue.popleft()
         if x == n-1 and y == m-1:
-            return dist
-        
+            return distance
+
         for i in range(4):
-            nx,ny = x + dx[i], y + dy[i]
-            
-            if 0 <= nx < n and 0 <=ny < m and (nx,ny) not in visited and maps[nx][ny]== 1:
-                queue.append((nx,ny,dist + 1))
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx <  n  and 0 <= ny < m and maps[nx][ny] ==1 and  (nx,ny) not in visited:
+                queue.append((nx,ny,distance + 1))
                 visited.add((nx,ny))
-        
+
     return -1
