9feecbdab64ec216550f71c9de5d3880c7805d71
yoonseopkim <qc70045@gmail.com>
2024-10-26 19:30:40 +0900

    Title: 함수를 이용한 369 게임, Time: 122ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241026/함수를 이용한 369 게임/369-games-using-functions.py
@@ -0,0 +1,13 @@
+def sam(a,b):
+    res = 0
+    for i in range(a,b+1):
+        if i %3 == 0 or issam(i):
+            res += 1
+    return res
+
+def issam(i):
+    if (i // 10) %3 ==0 or (i % 10) % 3 ==0:
+        return True
+
+a,b = map(int,input().split())
+print(sam(a,b))
