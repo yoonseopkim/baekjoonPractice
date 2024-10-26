2baaea5c7262e2a3da962162a48acf2962eaa58c
yoonseopkim <qc70045@gmail.com>
2024-10-27 08:24:08 +0900

    Title: 2021년 날짜의 유무, Time: 105ms, Memory: 24MB, Status: Passed - CodeTree

--- 241027/2021년 날짜의 유무/with-or-without-2021.py
+++ 241027/2021년 날짜의 유무/with-or-without-2021.py
@@ -4,12 +4,18 @@ def a(m,d):
     if m in (1,3,5,7,8,10,12):
         if d <= 31:
             return 'Yes'
+        else:
+            return 'No'
     elif m in (4,6,9,11):
         if d <= 30:
             return 'Yes'
+        else:
+            return 'No'
     elif m == 2:
         if d <= 28:
             return 'Yes'
+        else:
+            return 'No'
     else:
         return 'No'
 
