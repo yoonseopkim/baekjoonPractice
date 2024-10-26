2e09c9c216969d0e8906eff1d299b8339e0da0c7
yoonseopkim <qc70045@gmail.com>
2024-10-27 06:08:12 +0900

    Title: 함수를 이용한 온전수 판별, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241027/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.py
@@ -0,0 +1,19 @@
+a,b = map(int,input().split())
+
+def on(a,b):
+    res = 0
+    for i in range(a,b + 1):
+        if onjun(i):
+            res += 1
+    return res
+
+def onjun(i):
+    if not i % 2 ==0:
+     
+        if not i % 10 ==5:
+        
+            if not i % 3 == 0 and i % 9 != 0:
+              
+                return True
+
+print(on(a,b))
